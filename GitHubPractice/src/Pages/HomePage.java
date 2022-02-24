c616195ca263b1bcfb451f1b3ca5bc398b489a12
balagopal <balagopalqa@gail.com>
2022-02-24 21:54:08 +0530

    modified some files

--- GitHubPractice/src/Pages/HomePage.java
+++ GitHubPractice/src/Pages/HomePage.java
@@ -3,5 +3,6 @@ package Pages;
 public class HomePage {
 public static void main(String[] args) {
 	System.out.println("new test12345");
+	System.out.println("Test56789876");
 }
 }
--- GitHubPractice/src/Pages/LoginPage.java
+++ GitHubPractice/src/Pages/LoginPage.java
@@ -1,5 +1,7 @@
 package Pages;
 
 public class LoginPage {
-
+public static void main(String[] args) {
+	System.out.println("Test897897897demo");
+}
 }
